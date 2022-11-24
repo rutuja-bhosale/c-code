230b5708decfb0f40fc98b7815c86f54bdc30263
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2022-11-24 22:48:02 +0530

    while

---
+++ multiplicationTableWhile.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int no=0, i=0;
+    printf("enter any number ");
+    scanf("%d",&no);
+
+    while(i<=10)
+    {
+        printf("%d\n",no*i);
+        i++;
+    
+    }
+    return 0;
+} 
