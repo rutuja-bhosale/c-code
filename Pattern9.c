7f7122c27800087610a4bda02681366b065476a4
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2022-12-14 21:35:16 +0530

    Pattern9

---
+++ Pattern9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void Pattern9(int,int);
+
+int main()
+{
+    int x=0,y=0;
+    printf("enter values of row and column");
+    scanf("%d%d",&x,&y);
+
+    Pattern9(x,y);
+    return 0;
+}
+
+void Pattern9(int row,int col)
+{
+    int i=0,j=0;
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=col;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
