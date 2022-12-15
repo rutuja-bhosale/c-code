793a6545a77c4660fb3287eb49508f4234f388fe
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2022-12-15 22:41:13 +0530

    Pattern13

---
+++ Pattern13.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void Pattern13(int,int);
+
+int main()
+{
+    int x=0,y=0;
+    printf("enter values of row and column");
+    scanf("%d%d",&x,&y);
+
+    Pattern13(x,y);
+    return 0;
+}
+
+void Pattern13(int row,int col)
+{
+    int i=0,j=0;
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=col;j++)
+        {
+            if(i==j)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
+}
