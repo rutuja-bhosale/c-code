b5fd490366d71778a2a14ca624980f5364aa53a4
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2022-12-20 19:39:09 +0530

    Pattern24

---
+++ Pattern24.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void Pattern24(int);
+
+int main()
+{
+    int x=0,y=0;
+    printf("enter values of row ");
+    scanf("%d",&x,&y);
+
+    Pattern24(x);
+    return 0;
+}
+
+void Pattern24(int row)
+{
+    int i=0,j=0;
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(i%2==0)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("%d",j);
+            }
+        }
+        printf("\n");
+    }
+}
