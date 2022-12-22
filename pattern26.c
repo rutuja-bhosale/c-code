0f1a331ced2e495e42549c427e62377ef352d94d
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2022-12-22 21:45:22 +0530

    patten26recursion

---
+++ pattern26.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void Pattern26(int);
+
+int main()
+{
+    int n=0;
+    printf("enter any number");
+    scanf("%d",&n);
+
+    Pattern26(n);
+    return 0;
+}
+
+void Pattern26(int no)
+{
+    int i=0;
+    static icnt=0;
+
+    if(no!=0)
+    {
+        printf("%d",no);
+
+        for(i=0;i<icnt;i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        icnt++;
+
+        Pattern26(no/10);
+    }
+}
