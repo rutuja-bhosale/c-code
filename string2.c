c3d09b121d5dba18573a1406e8b8d8397c16de2e
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2023-01-27 14:43:28 +0530

    division

---
+++ string2.c
@@ -0,0 +1,12 @@
+// write a program to accept class division from user and display it
+
+#include<stdio.h>
+
+int main()
+{
+    char division='\0';
+    printf("enter division name ");
+    scanf("%c",&division);
+    printf("%c\n",division);
+    return 0;
+}
