887df53bb82a8dd3662900df52ad93a31bd892af
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2023-01-30 17:24:13 +0530

    printNoOfChar

---
+++ string6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printNchar(const char*,int);
+
+int main()
+{
+    int no=0;
+    char arr[50]={'\0'};
+    printf("enter any string ");
+     scanf("%[^\n]",arr);
+    printf("enter any number ");
+    scanf("%d",&no);
+
+    printNchar(arr,no);
+    return 0;
+}
+
+void printNchar(const char*p,int n)
+{
+    while(n!=0 && *p!='\0')
+    {
+        printf("%c",*p);
+        p++;
+        n--;
+    }
+}
